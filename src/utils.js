de6252e15887a447ba04803d699b60ce300a50cd
hyx <754060604@qq.com>
2019-08-23 14:28:49 +0800

    update version to 1.0.2 add utils function

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import validator from 'el-form-validator';
+import {Message} from 'element-ui';
 
 /**
  * 生成form验证规则
@@ -37,8 +38,76 @@ export const generateUuid = (prefix = '', size = 6) => {
     return prefix + Math.random().toString(36).slice(2, size + 2);
 };
 
+/**
+ * 处理http返回结果
+ *
+ * @param conditions        {Function}
+ * @param response          {Object}
+ * @param showSuccess       {boolean}
+ * @param successMsg        {string}
+ * @param showError         {boolean}
+ * @param errorMsg          {string}
+ * @return                  {Promise<Promise<*>|*>}
+ */
+export const handelHttpResponse = async (conditions, response, showSuccess = false, successMsg = undefined, showError = true, errorMsg = undefined) => {
+    if (conditions && conditions()) {
+        if (showSuccess) {
+            Message.success(successMsg || response.data.msg || response.data.data || '操作成功');
+        }
+        return await response.data;
+    } else {
+        if (showError) {
+            Message.error(errorMsg || response.data.msg || response.data.data);
+        }
+        return Promise.reject(response);
+    }
+};
+
+/**
+ * 克隆数据
+ *
+ * @param object    {Object | Array}
+ * @return          {Object | Array}
+ */
+export const deepClone = (object) => {
+    let result = Array.isArray(object) ? [] : {};
+    for (let key in object) {
+        if (object.hasOwnProperty(key)) {
+            if (object[key] instanceof File) {
+                result[key] = object[key];
+            } else if (typeof object[key] === 'object' && object[key]) {
+                result[key] = deepClone(object[key]);
+            } else {
+                result[key] = object[key];
+            }
+        }
+    }
+    return result;
+};
+
+/**
+ *  已第一个参数对比obj
+ *
+ * @param object        {Object | Array}
+ * @param diffObject    {Object | Array}
+ * @return              {Object | Array}
+ */
+export const objectDiff = (object, diffObject) => {
+    let newObject = typeof object !== 'object' ? [] : {};
+    if (typeof object === typeof diffObject) {
+        for (let key in object) {
+            if (object[key] !== diffObject[key]) {
+                newObject[key] = object[key];
+            }
+        }
+    }
+    return newObject;
+};
 
 export default {
     generateRule,
     generateUuid,
+    handelHttpResponse,
+    deepClone,
+    objectDiff
 };
