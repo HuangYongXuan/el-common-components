c73209220da35056fd34fe45b75e274d57ad04c3
hyx <754060604@qq.com>
2019-09-09 15:40:47 +0800

    update version 1.3.0 增加组件

---
+++ src/components/table/MdTableColumnDatetime.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-table-column :label="label" :width="width" :prop="prop" :align="align">
+        <template slot-scope="scope">
+            <div v-html="html(scope.row)"></div>
+        </template>
+    </el-table-column>
+</template>
+
+<script>
+	export default {
+		name: "MdTableColumnDatetime",
+		props: {
+			label: String,
+			width: String,
+			prop: String,
+			align: String
+		},
+		methods: {
+			html (row) {
+				let datetime = row[this.prop];
+				let arr = datetime.split(' ');
+				let html = '';
+				for (let k in arr) {
+					html += `<p class="__p">${arr[k]}</p>`
+				}
+				return html;
+			}
+		}
+	}
+</script>
+
+<style>
+    .__p {
+        margin: 0;
+    }
+</style>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import MdDateTimeRangePicker from './components/date/MdDateTimeRangePicker';
+import MdDateTimeRangePickerAlone from './components/date/MdDateTimeRangePickerAlone';
+import MdPagination from './components/page/MdPagination';
+import MdTableColumnDatetime from './components/table/MdTableColumnDatetime';
+
+Vue.component('MdDateTimeRangePicker', MdDateTimeRangePicker);
+Vue.component('MdDateTimeRangePickerAlone', MdDateTimeRangePickerAlone);
+Vue.component('MdPagination', MdPagination);
+Vue.component('MdTableColumnDatetime', MdTableColumnDatetime);
